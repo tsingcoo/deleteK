7d3069ba7c3ee220344cbba75d17f1eaa14653e6
wangql <wangql@nlp.nju.edu.cn>
2016-06-07 14:47:41 +0800

    first commit

--- deleteK/main.cpp
+++ deleteK/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+void deleteK(string& str, int k){
+    int len;
+    while (k) {
+        len=(int)str.length();
+        for (int i=0; i<len-1; ++i) {
+            if (str[i]<str[i+1]) {
+                str.erase(i,1);
+                --k;
+                break;
+            }
+        }
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    string s="8574739";
+
+    deleteK(s, 2);
+    cout<<s<<endl;
+
     return 0;
 }
